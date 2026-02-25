a78804800fe61bae0a21e3c831fdbb4136325a91
Thapa-Pro <bachchan.k.t@gmail.com>
2026-02-25 16:45:56 +0100

    copyright added

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,36 +6,60 @@ export default function Footer() {
     <footer className={styles.footer}>
       <div className={styles.inner}>
         <div className={styles.content}>
-          <p className={styles.followText}>You can follow us</p>
+          <div className={styles.socialSection}>
+            <p className={styles.followText}>You can follow us</p>
 
-          <div className={styles.socials}>
-            <a
-              href="https://www.facebook.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="Facebook"
-            >
-              <Image src="/facebook.svg" alt="Facebook" width={70} height={40} />
-            </a>
+            <div className={styles.socials}>
+              <a
+                href="https://www.facebook.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Facebook"
+              >
+                <Image
+                  src="/facebook.svg"
+                  alt="Facebook"
+                  width={70}
+                  height={40}
+                />
+              </a>
 
-            <a
-              href="https://www.instagram.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="Instagram"
-            >
-              <Image src="/instagram.svg" alt="Instagram" width={70} height={40} />
-            </a>
+              <a
+                href="https://www.instagram.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Instagram"
+              >
+                <Image
+                  src="/instagram.svg"
+                  alt="Instagram"
+                  width={70}
+                  height={40}
+                />
+              </a>
 
-            <a
-              href="https://www.youtube.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="YouTube"
-            >
-              <Image src="/youtube.svg" alt="YouTube" width={70} height={40} />
-            </a>
+              <a
+                href="https://www.youtube.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="YouTube"
+              >
+                <Image
+                  src="/youtube.svg"
+                  alt="YouTube"
+                  width={70}
+                  height={40}
+                />
+              </a>
+            </div>
           </div>
+
+          <p className={styles.copyright}>
+            © 2026 Bil4You
+            <br />
+            Internship project by Bachchan Thapa
+          </p>
+          
         </div>
       </div>
     </footer>
